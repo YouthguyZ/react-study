154b4b081a1f8a78e0ccb5ea63854311bff5e146
youngguyz <2281300996@qq.com>
2022-10-08 22:28:39 +0800

    :rocket: react18以前用法、创建渲染元素

--- day1/react-demo/src/index.js
+++ day1/react-demo/src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// webpack 入口文件 =>main.js
+// react18 以前的用法
+// 导入文件
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 创造元素
+const h1=React.createElement('h1',null,'helloworld!!')
+// 渲染元素到指定容器
+ReactDOM.render(h1,document.querySelector('#root'))
